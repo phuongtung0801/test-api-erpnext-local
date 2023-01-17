d7e402afc6138e9827ce043a47aa696163101959
erpnext <erpnext@phuongtung0801-VivoBook-ASUSLaptop-X530FA-S530FA>
2023-01-17 08:36:11 +0700

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Test Api
+
+tung
+
+#### License
+
+tung
---
+++ test_api/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Test Api",
+			"type": "module",
+			"label": _("Test Api")
+		}
+	]
